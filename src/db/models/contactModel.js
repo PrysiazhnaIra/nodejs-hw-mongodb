e21eda2c8d68e058b053c9598d27f3e9af16b887
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-09-12 22:43:47 +0300

    added contactModel

---
+++ src/db/models/contactModel.js
@@ -0,0 +1,30 @@
+import { model, Schema } from 'mongoose';
+
+const contactsShema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const ContactsCollection = model('contacts', contactsShema);
