1ddaadf2a48203737092728ed4bb333972a3aa1f
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-09-22 13:42:58 +0300

    fixed message, added versionKey to Shema, upgraded controllers by using throw

--- src/db/models/contactModel.js
+++ src/db/models/contactModel.js
@@ -24,7 +24,7 @@ const contactsShema = new Schema(
       required: true,
     },
   },
-  { timestamps: true },
+  { timestamps: true, versionKey: false },
 );
 
 export const ContactsCollection = model('contacts', contactsShema);
