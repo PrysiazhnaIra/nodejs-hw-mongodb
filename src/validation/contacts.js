8b457d88d6ff0d1b295a41d52b08ab843aa5ccc6
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-09-24 20:20:14 +0300

    validateBody, validation of post and patch

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,7 +1,12 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Name should be a string',
+    'string.min': 'Name should have at least {#limit} characters',
+    'string.max': 'Name should have at most {#limit} characters',
+    'any.required': 'Name is required',
+  }),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().min(3).max(20).required(),
   isFavorite: Joi.boolean(),
@@ -10,4 +15,12 @@ export const createContactSchema = Joi.object({
     .max(20)
     .valid('work', 'home', 'personal')
     .required(),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().email().min(3).max(20),
+  isFavorite: Joi.boolean(),
+  contactType: Joi.string().min(3).max(20).valid('work', 'home', 'personal'),
 });
